8f3af02f3419d5b9ecc89e4226e1d093245b0653
santhosh9652 <santhushivanathuni@gmail.com>
2022-01-12 18:10:19 +0530

    cod implemented

--- COD.java
+++ COD.java
@@ -1,5 +1,6 @@
 class COD{  
     public static void main(String args[]){  
-     System.out.println("Implement cod functionality");  
+     System.out.println("Implement cod functionality"); 
+System.out.println("Implemeted done cod"); 	 
     }  
 }  
